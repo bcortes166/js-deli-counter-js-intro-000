6d8c07be570719e1112658237a8ce0fdcd3dc484
Brian Cortes <briancorts166@gmail.com>
2018-06-07 23:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeli = []
 var takeANumber = (katzDeliLine, name) => {
 
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 
