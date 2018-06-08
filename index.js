b066d5e740f0563ce77a0cdb97e41c298bfee551
Brian Cortes <briancorts166@gmail.com>
2018-06-08 00:32:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ var takeANumber = (katzDeliLine, name) => {
 var nowServing = katzDeliLine => {
 
   if (katzDeliLine.length === 0) {
+    
     return 'There is nobody waiting to be served!'
+    
   } else {
+    
   return `Currently serving ${katzDeliLine[0]}.`
   katzDeliLine.shift()
+  
   }
 }
