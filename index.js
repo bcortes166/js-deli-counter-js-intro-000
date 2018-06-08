63817fc028efc748848ddce2abe789b97256d7cb
Brian Cortes <briancorts166@gmail.com>
2018-06-08 02:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,13 +26,14 @@ var currentLine = deliLine => {
     return 'The line is currently empty.'
 
   } else {
-    var string = '';
-    var list = [];
-    var i;
-    var n;
+    
+    var string = ''; var list = []; var i; var n;
+    var stringAdd = function() {
+      string = string + '${n}. ${deliLine(i)},
+    } 
     for (i = 0; i < deliLine.length; i++) {
       n = n + 1;
-      string = string + '${n}. ${deliLine(i)}, '
+      
     }
 
   return `The line is currently: ${string}`
